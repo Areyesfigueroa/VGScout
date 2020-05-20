742377b5cd0f132d85d8961cfb60bb92a6503d49
Aliel Reyes <alielreyes@gmail.com>
2020-05-19 22:17:55 -0700

    Embedded youtube player based on selection

---
+++ src/components/YoutubePlayer/YoutubePlayer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from './YoutubePlayer.module.css';
+
+
+const youtubePlayer = (props) => {
+    let video = null;
+
+    if(props.videoId) {
+        video = (<iframe
+        title="video" 
+        src={`https://www.youtube-nocookie.com/embed/${props.videoId}`} 
+        frameBorder="0" 
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
+        allowFullScreen>
+        </iframe>)
+    } else {
+        video = (
+            <div>
+                <p>No Video Found... :(</p>
+            </div>
+        );
+    }
+
+    return (
+        <div className={classes.YoutubePlayer}>
+            {video}
+        </div>
+    );
+}
+
+export default youtubePlayer;
