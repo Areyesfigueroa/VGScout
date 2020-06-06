cd167703a3e842174ea58beefc91893bd57e616b
Aliel Reyes <alielreyes@gmail.com>
2020-06-06 12:26:41 -0700

    Forgot to include percentage number on the ratings. Just added it back.

--- src/components/Ratings/Ratings.js
+++ src/components/Ratings/Ratings.js
@@ -5,10 +5,10 @@ import classes from './Ratings.module.css';
 const ratings = (props) => {
 
     const ratings = {
-        exceptional: props.ratings.length > 0 ? props.ratings[0]:0,
-        recommended: props.ratings.length > 0 ? props.ratings[1]:0,
-        meh: props.ratings.length > 0 ? props.ratings[2]:0,
-        skip: props.ratings.length > 0 ? props.ratings[3]:0,
+        exceptional: props.ratings.length > 0 ? props.ratings[0].percent:0,
+        recommended: props.ratings.length > 0 ? props.ratings[1].percent:0,
+        meh: props.ratings.length > 0 ? props.ratings[2].percent:0,
+        skip: props.ratings.length > 0 ? props.ratings[3].percent:0
     }
 
     const metacriticScore = props.metacritic ? props.metacritic:0;
