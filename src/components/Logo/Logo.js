ba2c1bdaf8387ffa42bd7da7a569d823162ee720
Aliel Reyes <alielreyes@gmail.com>
2021-03-18 08:32:15 -0700

    Fixed images bug and updated latest packages.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import classes from './Logo.module.css';
+import React from "react";
+import classes from "./Logo.module.css";
+import logoImg from "../../assets/VGS_Logo_01.png";
 
 const logo = () => {
-    return (
-        <div className={classes.Logo}>
-            <img src={require("../../assets/VGS_Logo_01.png")} />
-        </div>
-    );
+  return (
+    <div className={classes.Logo}>
+      <img src={logoImg} alt="Logo" />
+    </div>
+  );
 };
 
 export default logo;
--- src/components/Ratings/Ratings.js
+++ src/components/Ratings/Ratings.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import classes from './Ratings.module.css';
-
+import React from "react";
+import classes from "./Ratings.module.css";
+import metacriticLogo from "../../assets/metacriticLogo.png";
 
 const ratings = (props) => {
+  const ratings = {
+    exceptional: 0,
+    recommended: 0,
+    meh: 0,
+    skip: 0,
+  };
 
-    const ratings = {
-        exceptional: 0,
-        recommended: 0,
-        meh: 0,
-        skip: 0
-    };
+  if (props.ratings.length > 0) {
+    Object.keys(ratings).forEach((key, i) => {
+      if (!props.ratings[i]) return;
+      ratings[key] = props.ratings[i].percent;
+    });
+  }
 
-    if(props.ratings.length > 0) {
-        Object.keys(ratings).forEach((key, i) => {
-            if(!props.ratings[i]) return;
-            ratings[key] = props.ratings[i].percent;
-        });
-    }
+  const metacriticScore = props.metacritic ? props.metacritic : 0;
+  const thumbsUp = parseInt(ratings.exceptional + ratings.recommended);
+  const thumbsDown = parseInt(ratings.meh + ratings.skip);
 
-    const metacriticScore = props.metacritic ? props.metacritic:0;
-    const thumbsUp = parseInt(ratings.exceptional + ratings.recommended);
-    const thumbsDown = parseInt(ratings.meh + ratings.skip);
-
-    return (
-        <div className={classes.Ratings}>
-            <div><img src={require("../../assets/metacriticLogo.png")} alt="Metacritic Logo"/><p>{metacriticScore}</p></div>
-            <div><ion-icon name="thumbs-up-sharp"></ion-icon><p>{thumbsUp}%</p></div>
-            <div><ion-icon name="thumbs-down-sharp"></ion-icon><p>{thumbsDown}%</p></div>
-        </div>
-    );
-}
+  return (
+    <div className={classes.Ratings}>
+      <div>
+        <img src={metacriticLogo} alt="Metacritic Logo" />
+        <p>{metacriticScore}</p>
+      </div>
+      <div>
+        <ion-icon name="thumbs-up-sharp"></ion-icon>
+        <p>{thumbsUp}%</p>
+      </div>
+      <div>
+        <ion-icon name="thumbs-down-sharp"></ion-icon>
+        <p>{thumbsDown}%</p>
+      </div>
+    </div>
+  );
+};
 
 export default ratings;
